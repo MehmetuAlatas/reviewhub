94eab02266d598c918f1e637bc7fa9bbea5a4e14
Mehmet Alatas <mehmetualatas@gmail.com>
2022-03-27 08:23:43 +0300

    made some changes in class

--- src/Firstreview.java
+++ src/Firstreview.java
@@ -1,5 +1,7 @@
 public class Firstreview {
     public static void main(String[] args) {
         System.out.println("github connection");
+        System.out.println("second command to a new commit");
+        System.out.println("got it");
     }
 }
